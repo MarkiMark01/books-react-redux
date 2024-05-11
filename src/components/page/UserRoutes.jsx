1f63095216997846320e076a213ef6b8ee32fb3e
MarkiMark01 <markishgroup@gmail.com>
2024-05-11 17:08:34 +0300

    added login to UserRoutes

--- src/components/page/UserRoutes.jsx
+++ src/components/page/UserRoutes.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import React, { Suspense, lazy } from "react";
 
+const Login = lazy(() => import("./Login/Login"));
 const Signin = lazy(() => import("./Signin/Signin"));
 
 const UserRoutes = () => {
   return (
     <Suspense fallback={<p>...Loading</p>}>
       <Routes>
+        <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Signin />} />
       </Routes>
     </Suspense>
