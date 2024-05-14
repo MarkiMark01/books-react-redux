ef1ead52d83ec4d96ed9f14efc85e837a803d709
MarkiMark01 <markishgroup@gmail.com>
2024-05-14 16:34:49 +0300

    added logout to authOperations

--- src/components/redux/auth/authOperations.js
+++ src/components/redux/auth/authOperations.js
@@ -33,3 +33,19 @@ export const login = createAsyncThunk(
         }
     }
 )
+export const logout = createAsyncThunk(
+    'auth/logout',
+    async (_, { rejectWithValue }) => {
+        try {
+            const result = await api.logout();
+            return result;
+        } catch ({ response }) {
+            const { status, data } = response;
+            const error = {
+                status,
+                message: data.message,
+            }
+            return rejectWithValue(error);
+        }
+    }
+)
