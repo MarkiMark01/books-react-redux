04b43f833bb29a74a3fb08198771cae8edc64e69
MarkiMark01 <markishgroup@gmail.com>
2024-05-17 11:01:02 +0300

    added current to App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import React from 'react';
 import './App.css';
+import React from 'react';
+import { useDispatch } from "react-redux";
 
+import { current } from "./components/redux/auth/authOperations";
 import UserRoutes from './components/page/UserRoutes';
 
 function App() {
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(current());
+  }, [dispatch]);
+
   return (
     <div className="App">
       <UserRoutes />
