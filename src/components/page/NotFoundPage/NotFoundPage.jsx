e3d7dd796d0a215242b545c3cf6da3402fc3ff2d
MarkiMark01 <markishgroup@gmail.com>
2024-05-23 18:54:20 +0300

    added useTranslation to NotFoundPage

--- src/components/page/NotFoundPage/NotFoundPage.jsx
+++ src/components/page/NotFoundPage/NotFoundPage.jsx
@@ -3,11 +3,13 @@ import { useTranslation } from "react-i18next";
 import styles from "./stylesNoPage.module.scss";
 
 const NotFoundPage = () => {
+  const { t } = useTranslation();
+
   return (
     <div className={styles.nopage}>
-      <p>"Oops, something went wrong... Error 404" 🕵️</p>
+      <p>{t("Oops, something went wrong... Error 404")} 🕵️</p>
       <section className={styles.nopageLink}>
-        <NavLink to="/">"Go to books"</NavLink>
+        <NavLink to="/">{t("Go to books")}</NavLink>
       </section>
     </div>
   );
