82efc9bd7452d3580c11eca2342d1053498c98cd
MarkiMark01 <markishgroup@gmail.com>
2024-05-23 18:53:07 +0300

    create NotFoundPage

---
+++ src/components/page/NotFoundPage/NotFoundPage.jsx
@@ -0,0 +1,16 @@
+import { NavLink } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import styles from "./stylesNoPage.module.scss";
+
+const NotFoundPage = () => {
+  return (
+    <div className={styles.nopage}>
+      <p>"Oops, something went wrong... Error 404" 🕵️</p>
+      <section className={styles.nopageLink}>
+        <NavLink to="/">"Go to books"</NavLink>
+      </section>
+    </div>
+  );
+};
+
+export default NotFoundPage;
