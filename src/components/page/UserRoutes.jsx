19b1b6db1df6407fe0091b21d7a160d6aae2a4ec
MarkiMark01 <markishgroup@gmail.com>
2024-05-24 10:13:56 +0300

    added NotFoundPage to UserRoutes

--- src/components/page/UserRoutes.jsx
+++ src/components/page/UserRoutes.jsx
@@ -4,6 +4,7 @@ import React, { Suspense, lazy } from "react";
 const Login = lazy(() => import("./Login/Login"));
 const Signin = lazy(() => import("./Signin/Signin"));
 const About = lazy(() => import("./About/About"));
+const NotFoundPage = lazy(() => import("./NotFoundPage/NotFoundPage"));
 
 const UserRoutes = () => {
   return (
@@ -12,6 +13,7 @@ const UserRoutes = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Signin />} />
         <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </Suspense>
   );
