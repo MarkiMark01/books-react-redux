6cb06033982d60350b0ab695fd7d6de302322201
MarkiMark01 <markishgroup@gmail.com>
2024-05-25 14:54:06 +0300

    create Footer

---
+++ src/components/page/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import styles from "./stylesFooter.module.scss";
+
+const Footer = () => {
+  return (
+    <>
+      <section className={styles.foot}></section>
+      <main className={styles.footer}>
+        <section>2024 @ by Markiyan Marych</section>
+      </main>
+    </>
+  );
+};
+export default Footer;
