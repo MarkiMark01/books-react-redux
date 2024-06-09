30d7947ade30130071b2c31a33bc739cfe82775a
MarkiMark01 <markishgroup@gmail.com>
2024-06-09 19:51:25 +0300

    added getNweBooks to api

---
+++ src/components/shared/api.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const instance = axios.create({
+    baseURL: "https://66068cdbbe53febb857e25cd.mockapi.io/api/b/books",
+});
+
+export const getNewBooks = async () => {
+    try {
+        const result = await instance.get("/");
+        return result.data;
+    } catch (error) {
+        console.error("Error fetching new contacts:", error);
+        throw error;
+    }
+};
