01573536874d6677fd335ff4191b468e3047112d
MarkiMark01 <markishgroup@gmail.com>
2024-06-10 12:22:48 +0300

    added booksReducer to Store

--- src/components/redux/store.js
+++ src/components/redux/store.js
@@ -1,10 +1,38 @@
 import { configureStore } from "@reduxjs/toolkit";
+import {
+    persistReducer,
+    persistStore,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-import authReducer from "./auth/authSlice";
+import authReducer from "./auth/auth-slice";
+import booksReducer from "./books/booksSlice";
+
+const authPersistConfig = {
+    key: "auth",
+    storage,
+    whitelist: ["token"],
+};
+
+const persistedAuthReducer = persistReducer(authPersistConfig, authReducer);
 
 export const store = configureStore({
     reducer: {
-        auth: authReducer,
+        auth: persistedAuthReducer,
+        books: booksReducer,
     },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
 });
+export const persistor = persistStore(store);
 
