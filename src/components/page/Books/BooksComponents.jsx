bc411ad27d4d38eabb3dc2b62f19ce8841d2ed18
MarkiMark01 <markishgroup@gmail.com>
2024-06-25 10:24:01 +0300

    added useDebounce to BooksComponents

--- src/components/page/Books/BooksComponents.jsx
+++ src/components/page/Books/BooksComponents.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
-import { Loader } from "../../shared/loader";
+import { Loader } from "../../shared/Loader";
 import styles from "./stylesBooks.module.scss";
 import useDebounce from "../../shared/hooks/useDebounce";
 
