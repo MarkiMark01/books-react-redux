1693912f22861314f5016b5db4c62a52e669335c
MarkiMark01 <markishgroup@gmail.com>
2024-06-25 10:25:33 +0300

    created useModal

---
+++ src/components/shared/hooks/useModal.js
@@ -0,0 +1,17 @@
+import { useEffect } from "react";
+
+const useModal = (onClose) => {
+    useEffect(() => {
+        const handleKeyDown = (e) => {
+            if (e.code === "Escape") {
+                onClose();
+            }
+        };
+
+        window.addEventListener("keydown", handleKeyDown);
+
+        return () => {
+            window.removeEventListener("keydown", handleKeyDown);
+        };
+    }, [onClose]);
+};
