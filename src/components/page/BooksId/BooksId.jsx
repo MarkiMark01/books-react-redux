6a7f31950bb0641ee1b6885dc16e1ec430303f98
MarkiMark01 <markishgroup@gmail.com>
2024-06-28 12:29:32 +0300

    added updated BookId

--- src/components/page/BooksId/BooksId.jsx
+++ src/components/page/BooksId/BooksId.jsx
@@ -5,6 +5,38 @@ import { addNewCart, getUniqueBooks } from "../../redux/books/booksOperations";
 import BooksIdComponents from "./BooksIdComponents";
 
 const BooksId = () => {
+  const uniqueBook = useSelector((state) => state.books.uniqueBook);
+  const cart = useSelector((state) => state.books.cart);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  const [quantity, setQuantity] = useState(1);
+  const [totalPrice, setTotalPrice] = useState(0);
+
+  useEffect(() => {
+    dispatch(getUniqueBooks(id));
+  }, [dispatch, id]);
+
+  useEffect(() => {
+    if (uniqueBook) {
+      calculateTotalPrice(quantity, uniqueBook.price);
+    }
+  }, [uniqueBook, quantity]);
+
+  const handleQuantity = (e) => {
+    const count = e.target.value;
+    let newQuantity;
+    if (count === "") {
+      newQuantity = "";
+    } else {
+      newQuantity = isNaN(count) ? 1 : Math.max(1, Math.floor(Number(count)));
+    }
+
+    setQuantity(newQuantity);
+    calculateTotalPrice(newQuantity, uniqueBook.price);
+  };
+
   const calculateTotalPrice = (quantity, price) => {
     const total = quantity * price;
     setTotalPrice(total.toFixed(2));
@@ -29,18 +61,15 @@ const BooksId = () => {
     }
   };
 
-  const handleQuantity = (e) => {
-    const count = e.target.value;
-    let newQuantity;
-    if (count === "") {
-      newQuantity = "";
-    } else {
-      newQuantity = isNaN(count) ? 1 : Math.max(1, Math.floor(Number(count)));
-    }
-
-    setQuantity(newQuantity);
-    calculateTotalPrice(newQuantity, uniqueBook.price);
-  };
+  // if (!uniqueBook) {
+  //   return (
+  //     <section className={styles.bookId__emptyBlock}>
+  //       <p className={styles.bookId__empty}>
+  //         The book has not been selected yet...
+  //       </p>
+  //     </section>
+  //   );
+  // }
 
   return (
     <BooksIdComponents
