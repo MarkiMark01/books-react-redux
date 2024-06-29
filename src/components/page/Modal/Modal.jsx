5f56b4082ee0892d3630440ee384612ea1d95159
MarkiMark01 <markishgroup@gmail.com>
2024-06-29 15:20:39 +0300

    added modalRoot & createPortal to Modal

--- src/components/page/Modal/Modal.jsx
+++ src/components/page/Modal/Modal.jsx
@@ -4,11 +4,28 @@ import styles from "./Modal.module.scss";
 import gift from "../../../Assets/thanks.jpg";
 import useModal from "../../shared/hooks/useModal";
 
-export default function ModalWindow() {
+const modalRoot = document.querySelector("#modal-root");
+
+export default function ModalWindow({ onClose }) {
+  useModal(onClose);
+
   const handleBackdropClick = (e) => {
     if (e.currentTarget === e.target) {
       onClose();
     }
   };
-  return createPortal(<div className={styles.modal__backdrop}></div>);
+
+  return createPortal(
+    <div className={styles.modal__backdrop} onClick={handleBackdropClick}>
+      <div className={styles.modal__content}>
+        <div className={styles.modal__giftBox}>
+          <img src={gift} alt="Gift" className={styles.gift} />
+          <button className={styles.modal__ok} onClick={handleBackdropClick}>
+            Ok
+          </button>
+        </div>
+      </div>
+    </div>,
+    modalRoot
+  );
 }
