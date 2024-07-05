dccf55743124f5e7ea52b2502ae60f9b603defd8
MarkiMark01 <markishgroup@gmail.com>
2024-07-05 11:00:03 +0300

    added CartComponents to Cart

--- src/components/page/Cart/Cart.jsx
+++ src/components/page/Cart/Cart.jsx
@@ -12,10 +12,12 @@ import { useNavigate } from "react-router-dom";
 import CartComponents from "./CartComponents";
 
 const Cart = () => {
-  const navigate = useNavigate();
+  const cart = useSelector((state) => state.books.cart) || [];
   const totalPrice = useSelector((state) =>
     state.books.cart.reduce((sum, item) => sum + parseFloat(item.totalPrice), 0)
   );
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const handleRemoveFromCart = (item) => {
     dispatch(deleteCart(item.id));
@@ -35,7 +37,17 @@ const Cart = () => {
   const goToBack = () => {
     navigate(-1);
   };
-  return <></>;
+
+  return (
+    <CartComponents
+      goToBack={goToBack}
+      goToHome={goToHome}
+      handleClearCart={handleClearCart}
+      cart={cart}
+      totalPrice={totalPrice}
+      handleRemoveFromCart={handleRemoveFromCart}
+    />
+  );
 };
 
 export default Cart;
