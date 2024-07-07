aea925ce530fdf56876cf1cb6ca481c777dc58d1
MarkiMark01 <markishgroup@gmail.com>
2024-07-07 12:33:33 +0300

    created BookId.test.js

---
+++ src/components/page/BooksId/BookId.test.js
@@ -0,0 +1,19 @@
+
+import React, { useState } from 'react';
+import { render, fireEvent, screen } from '@testing-library/react';
+
+const CalculateTotalComponent = ({ calculateTotal, handlePurchase }) => {
+    const [totalPrice, setTotalPrice] = useState('');
+
+    const handleCalculate = () => {
+        calculateTotal(3, 10, setTotalPrice);
+    };
+
+    return (
+        <div>
+            <button onClick={handleCalculate}>Calculate Total</button>
+            <span data-testid="total-price">{totalPrice}</span>
+            <button onClick={handlePurchase}>Purchase</button>
+        </div>
+    );
+};
