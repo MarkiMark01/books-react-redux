47c819254d9a048a69030197239013b1d8c910f7
MarkiMark01 <markishgroup@gmail.com>
2024-07-08 11:47:34 +0300

    added first test to Cart.test

--- src/components/page/Cart/Cart.test.js
+++ src/components/page/Cart/Cart.test.js
@@ -1,3 +1,10 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 
+const CartTotal = ({ cart }) => {
+    const totalSum = cart
+        .reduce((sum, item) => sum + parseFloat(item.totalPrice), 0)
+        .toFixed(2);
+
+    return <div>Total: ${totalSum}</div>;
+};
