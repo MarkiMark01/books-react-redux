19b71096f01632199c63b4970465830bd3350f20
MarkiMark01 <markishgroup@gmail.com>
2024-07-26 11:45:06 +0300

    added googleLogout to Logout

--- src/components/page/Header/Logout.jsx
+++ src/components/page/Header/Logout.jsx
@@ -17,10 +17,11 @@ import ua from "../../../Assets/ua.png";
 const LANGUAGE_KEY = "language";
 
 const Logout = () => {
-  const { name } = useSelector(getUser);
-
-  const { t, i18n } = useTranslation();
+  const user = useSelector(getUser);
+  const googleUser = useSelector(getGoogleUser);
+  const isGoogleLogin = useSelector(isGoogleAuth);
   const dispatch = useDispatch();
+  const { t, i18n } = useTranslation();
 
   const [selectedLanguage, setSelectedLanguage] = useState(i18n.language);
 
@@ -28,24 +29,48 @@ const Logout = () => {
     localStorage.setItem(LANGUAGE_KEY, selectedLanguage);
   }, [selectedLanguage]);
 
+  const onLogout = () => {
+    if (isGoogleLogin) {
+      dispatch(googleLogout());
+    } else {
+      dispatch(logout());
+    }
+  };
+
   const changeLanguage = (lng) => {
     i18n.changeLanguage(lng);
     setSelectedLanguage(lng);
   };
 
-  const onLogout = () => {
-    dispatch(logout());
-    // return <Navigate to={"/"} />;
-  };
-
   return (
     <section className={styles.logout}>
+      <section className={styles.btnFlags}>
+        <img
+          src={usa}
+          alt="USA flag"
+          aria-label="Change language to English"
+          className={styles.flags}
+          onClick={() => changeLanguage("en")}
+          style={{ opacity: selectedLanguage === "en" ? 0.6 : 1 }}
+        />
+        <img
+          src={ua}
+          alt="Ukraine flag"
+          aria-label="Change language to Ukrainian"
+          className={styles.flags}
+          onClick={() => changeLanguage("uk")}
+          style={{ opacity: selectedLanguage === "uk" ? 0.6 : 1 }}
+        />
+      </section>
       <img src={icon} alt="Icon" className={styles.logout__icon} />
-      <span className={styles.logout__name}>{name}</span>
+      <span className={styles.logout__name}>
+        {isGoogleLogin ? googleUser.name : user.name}
+      </span>
       <button type="button" onClick={onLogout} className={styles.logout__btn}>
-        Log out
+        {t("Log out")}
       </button>
     </section>
   );
 };
+
 export default Logout;
