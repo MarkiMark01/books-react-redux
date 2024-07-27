f69b4e4a9f0d282c7afa29d45cc36bf6a8e64c5b
MarkiMark01 <markishgroup@gmail.com>
2024-07-27 16:50:30 +0300

    update Register

--- src/components/page/Header/Register.jsx
+++ src/components/page/Header/Register.jsx
@@ -1,51 +1,64 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import styles from "./stylesHeader.module.scss";
 import { useTranslation } from "react-i18next";
+import usa from "../../../Assets/usa.png";
+import ua from "../../../Assets/ua.png";
 
 const Register = () => {
-    const { i18n } = useTranslation();
-    const [selectedLanguage, setSelectedLanguage] = useState(i18n.language);
+  const { t, i18n } = useTranslation();
+  const [selectedLanguage, setSelectedLanguage] = useState(i18n.language);
 
-    const changeLanguage = (lng) => {
-        i18n.changeLanguage(lng);
-        setSelectedLanguage(lng);
-    };
+  useEffect(() => {
+    localStorage.setItem("language", selectedLanguage);
+  }, [selectedLanguage]);
 
-    return (
-        <>
-            <header className={styles.headerRegister}>
-                <section className={styles.headerRegister2}>
-                    <NavLink to={"/"} className={styles.logo}>
-                        BookStore
-                    </NavLink>
-                    <nav className={styles.navRegister}>
-                        <section style={{ display: "flex" }}>
-                            <button
-                                onClick={() => changeLanguage("en")}
-                                disabled={selectedLanguage === "en"}
-                            >
-                                EN
-                            </button>
-                            <button
-                                onClick={() => changeLanguage("uk")}
-                                disabled={selectedLanguage === "uk"}
-                            >
-                                UKR
-                            </button>
-                        </section>
-                        <section className={styles.navReg}>
-                            <NavLink to="/login">Log in </NavLink>
-                        </section>
-                        |
-                        <section className={styles.navReg}>
-                            <NavLink to="/register"> Sign up</NavLink>
-                        </section>
-                    </nav>
-                </section>
-            </header>
-            <section className={styles.head}></section>
-        </>
-    );
+  const changeLanguage = (lng) => {
+    i18n.changeLanguage(lng);
+    setSelectedLanguage(lng);
+  };
+
+  return (
+    <>
+      <header className={styles.headerRegister}>
+        <section className={styles.headerRegister2}>
+          <NavLink to={"/"} className={styles.logo}>
+            BookStore
+          </NavLink>
+          <nav className={styles.navRegister}>
+            <section style={{ display: "flex" }}>
+              <section className={styles.btnFlags}>
+                <img
+                  src={usa}
+                  alt="USA flag"
+                  className={styles.flags}
+                  onClick={() => changeLanguage("en")}
+                  style={{ opacity: selectedLanguage === "en" ? 0.6 : 1 }}
+                />
+                <img
+                  src={ua}
+                  alt="UA flag"
+                  className={styles.flags}
+                  onClick={() => changeLanguage("uk")}
+                  style={{ opacity: selectedLanguage === "uk" ? 0.6 : 1 }}
+                />
+              </section>
+            </section>
+            <section className={styles.navReg}>
+              <section style={{ marginRight: "5px" }}>
+                <NavLink to="/login">{t("Log in")}</NavLink>
+              </section>
+              |
+              <section style={{ marginLeft: "5px" }}>
+                <NavLink to="/register">{t("Sign up")}</NavLink>
+              </section>
+            </section>
+          </nav>
+        </section>
+      </header>
+      <section className={styles.head}></section>
+    </>
+  );
 };
+
 export default Register;
