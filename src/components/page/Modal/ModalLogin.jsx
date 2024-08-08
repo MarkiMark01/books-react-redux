cd47e9fa208bed56205640ed9193027f9d3fa412
MarkiMark01 <markishgroup@gmail.com>
2024-08-08 11:42:13 +0300

    added handleBackdropClick to ModalLogin

--- src/components/page/Modal/ModalLogin.jsx
+++ src/components/page/Modal/ModalLogin.jsx
@@ -2,3 +2,15 @@ import React from "react";
 import { createPortal } from "react-dom";
 import styles from "./stylesModal.module.scss";
 import useModal from "../../shared/hooks/useModal";
+
+
+const modalRoot = document.querySelector("#modal-root");
+
+export default function ModalWindow({ onClose }) {
+  useModal(onClose);
+
+  const handleBackdropClick = (e) => {
+    if (e.currentTarget === e.target) {
+      onClose();
+    }
+  };
