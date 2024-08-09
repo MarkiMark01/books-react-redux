dcb500188578521373d620cdf364d99d7853de88
MarkiMark01 <markishgroup@gmail.com>
2024-08-09 12:56:12 +0300

    created stylesModalCart

--- src/components/page/Modal/ModalCart.jsx
+++ src/components/page/Modal/ModalCart.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createPortal } from "react-dom";
-import styles from "./Modal.module.scss";
+import styles from "./stylesModalCart.module.scss";
 import gift from "../../../Assets/thanks.jpg";
 import useModal from "../../shared/hooks/useModal";
 
