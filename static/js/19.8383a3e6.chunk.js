25e3a4d2b665507a04b8d70a185930064d081ddb
MarkiMark01 <markishgroup@gmail.com>
2024-08-15 16:36:14 +0300

    Updates

---
+++ static/js/19.8383a3e6.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkbooks_react_redux=self.webpackChunkbooks_react_redux||[]).push([[19],{2019:(s,e,r)=>{r.r(e),r.d(e,{default:()=>c});var t=r(5239),u=r(3216),a=r(579);const c=()=>(0,t.A)()?(0,a.jsx)(u.C5,{to:"/"}):(0,a.jsx)(u.sv,{})}}]);
+//# sourceMappingURL=19.8383a3e6.chunk.js.map
