958a98a610852c59ea98a429b819da75e939dc7a
MarkiMark01 <markishgroup@gmail.com>
2024-08-17 18:20:11 +0300

    updated index in Project

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { store } from "./components/redux/store";
 import App from "./App";
 
@@ -9,8 +9,8 @@ import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
 );
