038e1b1682e4d508df1a7bfacaf534a66b22c563
MarkiMark01 <markishgroup@gmail.com>
2024-08-19 16:41:13 +0300

    updated the Project

--- src/components/page/BooksId/BooksId.jsx
+++ src/components/page/BooksId/BooksId.jsx
@@ -43,33 +43,18 @@ const BooksId = () => {
   };
 
   const handlePurchase = () => {
-    const isBookInCart = cart.some((item) => item.id === uniqueBook.id);
-    if (isBookInCart) {
-      // toggleModalOpen();
-    } else {
-      dispatch(
-        addNewCart({
-          id: uniqueBook.id,
-          title: uniqueBook.title,
-          price: uniqueBook.price,
-          quantity: quantity,
-          totalPrice: totalPrice,
-        })
-      );
-      setQuantity(1);
-      navigate("/cart");
-    }
+    dispatch(
+      addNewCart({
+        id: uniqueBook.id,
+        title: uniqueBook.title,
+        price: uniqueBook.price,
+        quantity: quantity,
+        totalPrice: totalPrice,
+      })
+    );
+    setQuantity(1);
+    navigate("/cart");
   };
-
-  // if (!uniqueBook) {
-  //   return (
-  //     <section className={styles.bookId__emptyBlock}>
-  //       <p className={styles.bookId__empty}>
-  //         The book has not been selected yet...
-  //       </p>
-  //     </section>
-  //   );
-  // }
 
   return (
     <BooksIdComponents
